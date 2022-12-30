1e259dddcfd0bfbc79a57998998cd20b801cf410
AntaresMugisho <antaresmugisho@gmail.com>
2022-12-30 19:53:32 +0200

    Allow auto update years on footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,21 @@
 
 
-//+--------------------------------+
-//|             |
-//+--------------------------------+
+// +--------------------------------+
+// | Auto update footer years       |
+// +--------------------------------+
+
+let start_year = 2022
+let years_label = document.querySelector(".years")
+let year = (new Date).getFullYear()
+
+// If user time is incorrect, then use UTC time
+if (year < start_year){
+    year = (new Date).getUTCFullYear()
+}
+
+if (year == start_year){
+    years_label.innerText = start_year
+}
+else{
+    years_label.innerText = `${start_year} - ${year}`
+}
