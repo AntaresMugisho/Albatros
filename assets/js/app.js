205cec95b06c30f4dbb1a6112ad4bac24c9aafc0
AntaresMugisho <antaresmugisho@gmail.com>
2022-12-31 12:34:41 +0200

    Add possibility to show/hide navmenu on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,26 @@ if (year == start_year){
 else{
     years_label.innerText = `${start_year} - ${year}`
 }
+
+// +--------------------------------+
+// | Show/Hide navmenu on mobile    |
+// +--------------------------------+
+
+// Humburger menu toggle on mobile
+const humburger = document.querySelector(".humburger")
+
+
+humburger.addEventListener("click", toggle_menu)
+
+function toggle_menu(){
+    const navmenu = document.querySelector(".nav-menu")
+    navmenu.classList.toggle("nav-menu--show")
+    humburger.classList.toggle("bi-list")
+    humburger.classList.toggle("bi-x")
+}
+
+// Hide navmenu when clicking on a link of navmenu
+const nav_links = document.getElementsByClassName("nav-link")
+for (const nav_link of nav_links){
+    nav_link.addEventListener("click", toggle_menu)
+}
